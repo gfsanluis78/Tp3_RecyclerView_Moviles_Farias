81dff7c2dfe04777c81323bd7278781b36082d11
gfsanluis78 <71910385+gfsanluis78@users.noreply.github.com>
2021-09-10 02:45:59 -0300

    Se termina primera parte RecyclerView ok

--- app/src/main/java/com/farias/tp3_recyclerview_moviles_farias/RecyclerAdapterPeliculas.java
+++ app/src/main/java/com/farias/tp3_recyclerview_moviles_farias/RecyclerAdapterPeliculas.java
@@ -1,22 +1,28 @@
 package com.farias.tp3_recyclerview_moviles_farias;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecyclerAdapterPeliculas extends RecyclerView.Adapter<RecyclerAdapterPeliculas.PeliculasViewHolder>{
 
-    private int mNumeroItems;
     final private ListItemClick miOnclickListener;
+    private List<Pelicula> lista;
 
-    public RecyclerAdapterPeliculas( int numeroItems, ListItemClick listener) {
-        mNumeroItems =  numeroItems;
+    public RecyclerAdapterPeliculas(List<Pelicula> objetos ,ListItemClick listener) {
         miOnclickListener = listener;
+        lista = objetos;
     }
 
     public interface  ListItemClick{
@@ -43,28 +49,46 @@ public class RecyclerAdapterPeliculas extends RecyclerView.Adapter<RecyclerAdapt
 
     @Override
     public void onBindViewHolder(@NonNull PeliculasViewHolder holder, int position) {
-        holder.bind(position);
+        holder.titulo.setText(lista.get(position).getTitulo());
+        holder.resenia.setText(lista.get(position).getResenia());
+        holder.IV_foto.setImageResource(lista.get(position).getImagenCartelera());
+        holder.BT_detalles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("mensaje","llego ");
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
-        return mNumeroItems;
+        return lista.size();
     }
 
 
     class PeliculasViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        TextView mTVlistaPeliculasView;
+        Button BT_detalles;
+        TextView resenia;
+        TextView titulo;
+        ImageView IV_foto;
+
 
         public PeliculasViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            mTVlistaPeliculasView = itemView.findViewById(R.id.TV_lista_peliculas);
+            BT_detalles = itemView.findViewById(R.id.BT_detalles);
+            resenia = itemView.findViewById(R.id.TV_resenia);
+            titulo = itemView.findViewById(R.id.TV_titulo);
+            IV_foto = itemView.findViewById(R.id.IV_foto);
             itemView.setOnClickListener(this);
         }
 
         void bind (int listaIndex){
-            mTVlistaPeliculasView.setText(String.valueOf(listaIndex));
+            titulo.setText(String.valueOf(listaIndex));
+
+
         }
 
         @Override
